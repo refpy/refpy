23c721cf14ce048f1c38943580d0d47e71bdf926
ismael-ripoll <ismael.ripoll@subseaenergies.com>
2025-01-23 09:48:52 +0000

    Manual

--- docs/_build/html/_sources/modules.rst.txt
+++
@@ -1,9 +0,0 @@
-#############
-API Reference
-#############
-
-.. toctree::
-   :maxdepth: 4
-
-   refpy_pipe
-   refpy_lb
--- docs/conf.py
+++ docs/conf.py
@@ -6,30 +6,23 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-import os
-import sys
-
-sys.path.insert(0, os.path.abspath("../source"))
-
 project = 'RefPy'
 copyright = '2025, Ismael Ripoll'
 author = 'Ismael Ripoll'
-release = 'v0.1.3'
+release = 'v0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    'sphinx.ext.autodoc',
-]
+extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
-
-html_baseurl = '/'
