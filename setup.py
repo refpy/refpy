b64e48e225806117544419ec02ed17a65abbf988
ismael-ripoll <195974880+ismael-ripoll@users.noreply.github.com>
2025-08-26 16:10:49 +0100

    Bump version to 0.1.21 and update Python requirement to >=3.10

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="refpy",
-    version="0.1.20",
+    version="0.1.21",
     description="Package for subsea pipelines and risers design in Python",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
@@ -15,7 +15,7 @@ setup(
         "Operating System :: OS Independent",
     ],
     packages=find_packages(),
-    python_requires='>=3.11',
+    python_requires='>=3.10',
     install_requires=[],
     project_urls={
         "Documentation": "https://refpy.github.io/refpy",
